93fc7871fb7c0a8b81f9d33d23537a6b941b01a1
Ilhom Maksadkulov <ilhommaksadkulov@Ilhoms-MacBook-Pro.local>
2024-07-20 15:09:29 +0500

    feature:wrap Nest.js application to AWS Lambda

---
+++ deploy/Stack.ts
@@ -0,0 +1,38 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as nodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+import { RDS_CONNECTION_URL } from './config';
+
+export class Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: cdk.StackProps) {
+    super(scope, id, props);
+
+    const server = new nodejs.NodejsFunction(this, 'server', {
+      functionName: 'nodejs-aws-cart-api',
+      entry: 'dist/main.lambda.js',
+      timeout: cdk.Duration.seconds(30),
+      memorySize: 1024,
+      runtime: lambda.Runtime.NODEJS_16_X,
+      environment: {
+        RDS_CONNECTION_URL: RDS_CONNECTION_URL,
+      },
+      bundling: {
+        externalModules: [
+          '@nestjs/microservices',
+          '@nestjs/websockets',
+          'class-transformer',
+          'class-validator',
+        ],
+      },
+    });
+
+    // exposes the lambda function via HTTP URL
+    const { url } = server.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: { allowedOrigins: ['*'] },
+    });
+
+    new cdk.CfnOutput(this, 'Url', { value: url });
+  }
+}
