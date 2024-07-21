7c9ad1bf617645b24ef047915497a2aaa969574e
Ilhom Maksadkulov <ilhommaksadkulov@Ilhoms-MacBook-Pro.local>
2024-07-21 11:52:27 +0500

    refactor: remove commented code

--- src/cart/models/cart.entity.ts
+++ src/cart/models/cart.entity.ts
@@ -14,13 +14,6 @@ export class Cart {
   @Column({type: 'uuid'})
   user_id: string;
 
-  //TODO: remove commented code
-  // @Column({ type: 'varchar', length: 255 })
-  // created_at: string;
-
-  // @Column({ type: 'varchar', length: 255 })
-  // updated_at: string;
-
   @CreateDateColumn({ type: 'timestamp' })
   created_at: Date;
 
